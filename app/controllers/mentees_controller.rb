3ca8864f2b69945d5e0b302cc8945690f598b3f4
david-commit <davidondiege@gmail.com>
2023-03-01 11:18:07 +0300

    Added Mentee methods and uniqueness validator

--- app/controllers/mentees_controller.rb
+++ app/controllers/mentees_controller.rb
@@ -1,2 +1,37 @@
 class MenteesController < ApplicationController
+
+ # Render all Mentees
+ def index
+  # INCOMPLETE - Only Amin Role should have access
+  mentees = Mentee.all
+  render json mentees, status: :ok
+ end
+
+ # Render specific user profile
+ def show
+  # INCOMPLETE - Only logged in Mentee userType should have Access
+  mentee = Mentee.find_by(email: params[:email])
+  if mentee
+   render json: mentee, status: :ok   
+  end
+ end
+
+ # Allows a new user to craete an account
+ def create
+  mentee = Mentee.create!(mentee_params)
+  if mentee
+   render json: mentee, status: :created
+  end
+ end
+
+ # User self destruct his/her account
+ def destroy
+  # INCOMPLETE - Check for user session before destroy
+  mentee = Mentee.find_by(email: params[:email])
+  if mentee
+   mentee.destroy
+   head :no_content
+  end
+ end
+
 end
--- app/models/mentee.rb
+++ app/models/mentee.rb
@@ -2,7 +2,7 @@ class Mentee < ApplicationRecord
  has_secure_password
  validates :full_name, presence: true
  validates :dob, presence: true
- validates :email, presence: true
+ validates :email, presence: true, uniqueness: true
  validates :phone, presence: true
  validates :bio, presence: true, length: { minimum: 20 }
  validates :title, presence: true
