efe97ed6c129f1c429ca4de52ee85daa278fd41e
david-commit <davidondiege@gmail.com>
2023-03-01 10:55:44 +0300

    Added validations to the mentee model

--- app/models/mentee.rb
+++ app/models/mentee.rb
@@ -1,2 +1,14 @@
 class Mentee < ApplicationRecord
+ has_secure_password
+ validates :full_name, presence: true
+ validates :dob, presence: true
+ validates :email, presence: true
+ validates :phone, presence: true
+ validates :bio, presence: true, length: { minimum: 20 }
+ validates :title, presence: true
+ validates :gender, presence: true, inclusion: { in: ["Male", "Female", "Prefer not to say"] }
+ validates :country, presence: true
+ validates :language, presence: true
+ validates :expertise, presence: true
+ validates :top_goal, presence: true
 end
--- db/migrate/20230301070726_create_mentees.rb
+++ db/migrate/20230301070726_create_mentees.rb
@@ -8,10 +8,10 @@ class CreateMentees < ActiveRecord::Migration[7.0]
       t.text :bio
       t.string :title
       t.string :organization
+      t.string :gender
       t.string :country
       t.string :language
       t.string :expertise
-      t.string :gender
       t.string :top_goal
 
       t.timestamps
